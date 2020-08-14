42cec6874f40d781f0eb62c9436ec5fb2a252364
macycle <1124942097@qq.com>
2020-08-14 16:33:38 +0800

    添加icon和iconPosition属性

--- src/button.vue
+++ src/button.vue
@@ -1,9 +1,17 @@
 <template>   
-        <button class="am-button">按钮</button>
+        <button class="am-button" :class="{[`icon-${iconPosition}`]:true}">
+            <svg v-if="icon" class="icon">
+                <use :xlink:href="`#am-${icon}`"></use>
+            </svg>
+            <div class="content">
+                <slot></slot>
+            </div>
+        </button>
 </template>
 
 <script>
     export default {
+        props:['icon',`iconPosition`]
     }
 </script>
 
@@ -15,14 +23,21 @@
     border-radius: var(--border-radius);
     border: 1px solid var(--border-color);
     background: var(--button-bg);
-    &:hover{
-        border-color: var(--border-color-hover);
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    vertical-align: middle;
+    >.icon{order:1; width:1em; height: 1em;margin-right: .3em}
+    >.content{order:2}
+    &.icon-right{    /*后面的样式会覆盖前面的样式 */
+        >.icon{order:2;margin-right: 0; margin-left: .3em}
+        >.content{order:1}
     }
-    &:active{
-        background-color: var(--button-active-bg);
-    }
-    &:focus{
-        outline: none;
-    }
+    &:hover{border-color: var(--border-color-hover);}
+    &:active{background-color: var(--button-active-bg);}
+    &:focus{outline: none;}
 }
+
+
 </style>
+
