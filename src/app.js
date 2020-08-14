b035bbd27c0197663bbd0c5f22c555ee49cc21ff
macycle <1124942097@qq.com>
2020-08-14 20:47:16 +0800

    完成button-group组件

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import  Button from './button.vue';
-import Icon from './icon.vue'
+import Icon from './icon.vue';
+import ButtonGroup from './button-group.vue';
 Vue.component('am-button',Button);
 Vue.component('am-icon',Icon);
+Vue.component('am-group-button',ButtonGroup)
 
 new Vue({
     el:'#app',
